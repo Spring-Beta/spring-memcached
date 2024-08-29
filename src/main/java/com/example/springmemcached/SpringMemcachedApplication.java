32e36ae48320010d05a58a51f3650413946dbbf2
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-08-29 19:02:21 +0600

    memcached initiallized

---
+++ src/main/java/com/example/springmemcached/SpringMemcachedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmemcached;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMemcachedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMemcachedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmemcached/SpringMemcachedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmemcached;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemcachedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
